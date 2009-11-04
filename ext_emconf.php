13ab7b6e0bf4066f3acce33c40ce2548df56252a
michael.klapper <michael.klapper@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-11-04 22:42:18 +0000

    Minor code change
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/be_groups/trunk@26165 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "be_groups"
 #
-# Auto generated 03-11-2009 20:22
+# Auto generated 04-11-2009 23:41
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => 'AOE media GmbH',
-	'version' => '0.1.0,
+	'version' => '0.1.0',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:26:{s:9:"ChangeLog";s:4:"9cff";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"4660";s:14:"ext_tables.sql";s:4:"4403";s:16:"locallang_db.xml";s:4:"f967";s:46:"selicon_be_groups_tx_begroups_kind_0.gif";s:4:"b050";s:49:"selicon_be_groups_tx_begroups_kind_0__h.gif";s:4:"4841";s:46:"selicon_be_groups_tx_begroups_kind_1.gif";s:4:"79e4";s:49:"selicon_be_groups_tx_begroups_kind_1__h.gif";s:4:"4841";s:46:"selicon_be_groups_tx_begroups_kind_2.gif";s:4:"b321";s:49:"selicon_be_groups_tx_begroups_kind_2__h.gif";s:4:"4841";s:46:"selicon_be_groups_tx_begroups_kind_3.gif";s:4:"4301";s:49:"selicon_be_groups_tx_begroups_kind_3__h.gif";s:4:"4841";s:46:"selicon_be_groups_tx_begroups_kind_4.gif";s:4:"b806";s:49:"selicon_be_groups_tx_begroups_kind_4__h.gif";s:4:"4841";s:46:"selicon_be_groups_tx_begroups_kind_5.gif";s:4:"afdd";s:49:"selicon_be_groups_tx_begroups_kind_5__h.gif";s:4:"4841";s:46:"selicon_be_groups_tx_begroups_kind_6.gif";s:4:"0588";s:49:"selicon_be_groups_tx_begroups_kind_6__h.gif";s:4:"4841";s:46:"selicon_be_groups_tx_begroups_kind_7.gif";s:4:"038f";s:49:"selicon_be_groups_tx_begroups_kind_7__h.gif";s:4:"4841";s:46:"selicon_be_groups_tx_begroups_kind_8.gif";s:4:"26ba";s:49:"selicon_be_groups_tx_begroups_kind_8__h.gif";s:4:"4841";s:19:"doc/wizard_form.dat";s:4:"f5f8";s:20:"doc/wizard_form.html";s:4:"1e97";}',
+	'_md5_values_when_last_written' => 'a:31:{s:9:"ChangeLog";s:4:"3254";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"0667";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"1022";s:14:"ext_tables.php";s:4:"ff11";s:14:"ext_tables.sql";s:4:"ee50";s:16:"locallang_db.xml";s:4:"55f2";s:40:"selicon_be_groups_tx_begroups_kind_0.gif";s:4:"b050";s:43:"selicon_be_groups_tx_begroups_kind_0__h.gif";s:4:"4841";s:40:"selicon_be_groups_tx_begroups_kind_1.gif";s:4:"79e4";s:43:"selicon_be_groups_tx_begroups_kind_1__h.gif";s:4:"4841";s:40:"selicon_be_groups_tx_begroups_kind_2.gif";s:4:"b321";s:43:"selicon_be_groups_tx_begroups_kind_2__h.gif";s:4:"4841";s:40:"selicon_be_groups_tx_begroups_kind_3.gif";s:4:"4301";s:43:"selicon_be_groups_tx_begroups_kind_3__h.gif";s:4:"4841";s:40:"selicon_be_groups_tx_begroups_kind_4.gif";s:4:"b806";s:43:"selicon_be_groups_tx_begroups_kind_4__h.gif";s:4:"4841";s:40:"selicon_be_groups_tx_begroups_kind_5.gif";s:4:"afdd";s:43:"selicon_be_groups_tx_begroups_kind_5__h.gif";s:4:"4841";s:40:"selicon_be_groups_tx_begroups_kind_6.gif";s:4:"0588";s:43:"selicon_be_groups_tx_begroups_kind_6__h.gif";s:4:"4841";s:40:"selicon_be_groups_tx_begroups_kind_7.gif";s:4:"038f";s:43:"selicon_be_groups_tx_begroups_kind_7__h.gif";s:4:"4841";s:40:"selicon_be_groups_tx_begroups_kind_8.gif";s:4:"26ba";s:43:"selicon_be_groups_tx_begroups_kind_8__h.gif";s:4:"4841";s:50:"service/class.tx_begroups_service_tcemain_hook.php";s:4:"3eed";s:59:"service/class.user_begroups_service_tcaform_labelHelper.php";s:4:"195a";s:21:"service/locallang.xml";s:4:"92c0";s:19:"doc/wizard_form.dat";s:4:"23a1";s:20:"doc/wizard_form.html";s:4:"83e0";}',
 	'suggests' => array(
 	),
 );
